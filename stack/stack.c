8f0021bfb1a649bc95ee3ef713a4fa509315c609
asskicker54 <ass_kicker54@vk.com>
2024-09-04 17:03:53 +0700

    redefinded stack / first list than stack

---
+++ stack/stack.c
@@ -0,0 +1,9 @@
+#include "stack.h"
+
+stack_t *NewStack(int data) {
+  // TODO: Implement NewStack function
+}
+
+void DestructStack(stack_t *stack) {
+  // TODO: Implenet DestructStack function
+}
--- stack/stack.h
+++ stack/stack.h
@@ -1,21 +1,18 @@
 #ifndef STACK_H
 #define STACK_H
 
+typedef struct Node {
+  // FIX: create own type for data field
+  int data;
+  struct Node *previous;
+} stackNode_t;
+
 typedef struct Stack {
-    struct Node {
-        int data;
-        Node* previous;
-    };
-    Node* top;
-    int length;
-}stack_t;
+  stackNode_t *topNode;
+  int length;
+} stack_t;
 
-stack_t* NewStack(int data);
-void DestructStack(stack_t* stack);
-
-int stackPush(stack_t* stack, int data);
-int stackPop(stack_t* stack, int& data);
-
-void getStackInfo(stack_t* stack);
+stack_t *NewStack(int data);
+void DestructStack(stack_t *stack);
 
 #endif
